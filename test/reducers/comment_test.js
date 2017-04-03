25876088f434a8b224da92fcfff230e7eca4733e
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2017-04-03 08:48:14 +0100

    Add comments reducer

---
+++ test/reducers/comment_test.js
@@ -0,0 +1,10 @@
+import { expect } from '../test_helper'
+
+import commentReducer from '../../src/reducers/comments'
+import { SAVE_COMMENT } from '../../src/actions/types'
+
+describe('Comments Reducer', () => {
+  it('handles action with unknown type', () => {
+    expect(commentReducer()).to.be.eql([])
+  })
+})
